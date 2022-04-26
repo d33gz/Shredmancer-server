5bf31d391176f00ae316b032097f25a082642bfa
Hendo1349 <hendo1349@gmail.com>
2022-04-26 09:15:07 -0400

    initial commit

---
+++ src/main/java/revature/d33gz/shredmancer/server/ShredmancerServerApplication.java
@@ -0,0 +1,13 @@
+package revature.d33gz.shredmancer.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShredmancerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShredmancerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/revature/d33gz/shredmancer/server/ShredmancerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package revature.d33gz.shredmancer.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShredmancerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
